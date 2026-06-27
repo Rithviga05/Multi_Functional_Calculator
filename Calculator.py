0debfdc65b8dcddacee545a8a5848c3bb879d113
Rithviga05 <rithvigavd@gmail.com>
2026-06-27 11:00:49 +0530

    Implement basic calculator functions in Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -10,9 +10,9 @@ if  b != 0:
  print("Percentage  is: " ,a/b*100 ,"%")
 if a>0 and  b>0 and  b != 1:
  print("log (base b) of a:  ", math.log(a,b))
-print("log(base 10) of a: ", math.log10(a))
-print("natural log (ln) of a: ", math.log(a))
-print("Power of a: ",a**b)
+ print("log(base 10) of a: ", math.log10(a))
+ print("natural log (ln) of a: ", math.log(a))
+ print("Power of a: ",a**b)
 angle_in_radians =  math.radians(a)
 print("sin of a is: ",math.sin(angle_in_radians))
 print("cos of a is: ",math.cos(angle_in_radians))
